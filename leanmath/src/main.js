40456e612185ffb589b6eae97999463ef2730827
Jake <ljangwon@gmail.com>
2021-02-21 09:09:27 +0900

    fix

--- leanmath/src/main.js
+++ leanmath/src/main.js
@@ -26,12 +26,12 @@ function loadStudents() {
 function displayStudents(students) {
 	const container = document.querySelector('.students__list');
 	container.innerHTML = students
-		.map((student) => createHTMLString(student))
+		.map((student) => createHTMLString_st_list(student))
 		.join('');
 }
 
 // Create HTML list item from the given data item
-function createHTMLString(student) {
+function createHTMLString_st_list(student) {
 	return `
   <li class="student">
      <span class="item_description">${student.num}, ${student.grade1}, ${student.grade2}, ${student.name}</span><br>
@@ -42,6 +42,26 @@ function createHTMLString(student) {
      <input type="text" id="grade2" value="${student.grade2}">,
      <input type="text" id="name" value="${student.name}"></span>
      </form>
+  </li>
+  `;
+}
+
+// Update the dashboard with the given items
+function display_st_dashboard(students) {
+	const container = document.querySelector('#st__dashboard');
+	container.innerHTML = students
+		.map((student) => createHTMLString_st_dashboard(student))
+		.join('');
+}
+
+// Create HTML list item from the given data item
+function createHTMLString_st_dashboard(student) {
+	return `
+  <li class="student">
+     	<span class="item_description"> ${student.num}, 
+     	<input type="text" id="grade1" value="${student.grade1}">,
+     	<input type="text" id="grade2" value="${student.grade2}">,
+     	<input type="text" id="name" value="${student.name}"></span>
   </li>
   `;
 }
@@ -76,7 +96,8 @@ function setEventListeners(items) {
 
 loadStudents()
 	.then((students) => {
-		displayStudents(students);
+		//displayStudents(students);
+		display_st_dashboard(students);
 		setEventListeners(students);
 	})
 	.catch(console.log);
