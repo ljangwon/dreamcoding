445dd28dac53f54704ede7dcea6858bcbb8606a5
Jake <ljangwon@gmail.com>
2021-03-19 12:06:06 +0900

    datatable click event

--- javascript/array-api.js
+++ javascript/array-api.js
@@ -1,108 +1,105 @@
-function log(message){
-  console.log(message);
+function log(message) {
+	console.log(message);
 }
 // Q1. make a string out of an array
 {
-  const fruits = ['apple', 'banana', 'orange'];
-  const result = fruits.join('|');
+	const fruits = ['apple', 'banana', 'orange'];
+	const result = fruits.join('|');
 
-  log(result);
+	log(result);
 }
 
 // Q2. make an array out of a string
 {
-  const fruits = '🍎, 🥝, 🍌, 🍒';
-  const array1 = fruits.split(',');
-  console.log( array1);
+	const fruits = '🍎, 🥝, 🍌, 🍒';
+	const array1 = fruits.split(',');
+	console.log(array1);
 }
 
 // Q3. make this array look like this: [5, 4, 3, 2, 1]
 {
-  const array = [1, 2, 3, 4, 5];
-  array1 = array.reverse();
-  log( array1);
+	const array = [1, 2, 3, 4, 5];
+	array1 = array.reverse();
+	log(array1);
 }
 
 // Q4. make new array without the first two elements
 {
-  const array = [1, 2, 3, 4, 5];
-  log(array.slice(2));
+	const array = [1, 2, 3, 4, 5];
+	log(array.slice(2));
 }
 
 class Student {
-  constructor(name, age, enrolled, score) {
-    this.name = name;
-    this.age = age;
-    this.enrolled = enrolled;
-    this.score = score;
-  }
+	constructor(name, age, enrolled, score) {
+		this.name = name;
+		this.age = age;
+		this.enrolled = enrolled;
+		this.score = score;
+	}
 }
 const students = [
-  new Student('A', 29, true, 45),
-  new Student('B', 28, false, 80),
-  new Student('C', 30, true, 90),
-  new Student('D', 40, false, 66),
-  new Student('E', 18, true, 88),
+	new Student('A', 29, true, 45),
+	new Student('B', 28, false, 80),
+	new Student('C', 30, true, 90),
+	new Student('D', 40, false, 66),
+	new Student('E', 18, true, 88),
 ];
 
 // Q5. find a student with the score 90
 {
-  const result = students.find( ( student, index) => student.score === 90 );
-  console.log(result);
-  }
-
+	const result = students.find((student, index) => student.score === 90);
+	console.log(result);
+}
 
 // Q6. make an array of enrolled students
 {
-  const result = students.filter( (student) => student.enrolled );
-  log(result);
+	const result = students.filter((student) => student.enrolled);
+	log(result);
 }
 
 // Q7. make an array containing only the students' scores
 // result should be: [45, 80, 90, 66, 88]
 {
-  const result = students.map( (student) => student.score*2 );
-  log(result);
+	const result = students.map((student) => student.score * 2);
+	log(result);
 }
 
 // Q8. check if there is a student with the score lower than 50
 {
-  console.clear();
-  const result = students.some( (student) => student.score<50 );
-  log(result);
-  const result2 = students.every( (student) => student.score<50 );
-  log(result2);
+	console.clear();
+	const result = students.some((student) => student.score < 50);
+	log(result);
+	const result2 = students.every((student) => student.score < 50);
+	log(result2);
 }
 
 // Q9. compute students' average score
 {
-  const result = students.reduce( (prev, curr) =>{
-    log('----------');
-    log(prev);
-    log(curr);
-    return prev + curr.score;
-  }, 0)
-  log(result/students.length);
+	const result = students.reduce((prev, curr) => {
+		log('----------');
+		log(prev);
+		log(curr);
+		return prev + curr.score;
+	}, 0);
+	log(result / students.length);
 }
 
 // Q10. make a string containing all the scores
 // result should be: '45, 80, 90, 66, 88'
 {
-  const result = students
-  .map((student) => student.score)
-  .filter( (score)=> score >=40)
-  .join();
-  log(result);
+	const result = students
+		.map((student) => student.score)
+		.filter((score) => score >= 40)
+		.join();
+	log(result);
 }
 
 // Bonus! do Q10 sorted in ascending order
 // result should be: '45, 66, 80, 88, 90'
 {
-
-  const result = students
-  .map( (student) => student.score)
-  .sort()
-  .join();
-  log(result);
-
+	const result = students
+		.map((student) => student.score)
+		.sort()
+		.join();
+	log(result);
 }
